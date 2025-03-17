a38053ffd5a6c7dd193f8c70ed04baae5b3eb970
Curevo <Curevoapp@gmail.com>
2025-03-17 12:11:24 +0530

    Added Backend

---
+++ Backend/Curevo/src/main/java/com/certaint/curevo/CurevoApplication.java
@@ -0,0 +1,13 @@
+package com.certaint.curevo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurevoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurevoApplication.class, args);
+    }
+
+}
---
+++ Backend/Curevo/src/test/java/com/certaint/curevo/CurevoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certaint.curevo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurevoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
