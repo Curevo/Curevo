51be5f32ee1db4001f0baf86f921afa457844366
Rounak665 <rounakcstar665@gmail.com>
2025-03-17 23:04:05 +0530

    Added Application.yml,ensured env variables work properly

---
+++ Backend/Curevo/src/main/java/com/certaint/Utilities/EnvConfig.java
@@ -0,0 +1,14 @@
+package com.certaint.Utilities;
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class EnvConfig {
+    private static final Dotenv dotenv = Dotenv.load();
+
+    static {
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+    }
+
+    public static String get(String key) {
+        return dotenv.get(key);
+    }
+}
--- Backend/Curevo/src/main/java/com/certaint/curevo/CurevoApplication.java
+++ Backend/Curevo/src/main/java/com/certaint/curevo/CurevoApplication.java
@@ -1,13 +1,15 @@
 package com.certaint.curevo;
 
+import com.certaint.Utilities.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CurevoApplication {
-
     public static void main(String[] args) {
+        // Load environment variables from .env
+        new EnvConfig();
+
         SpringApplication.run(CurevoApplication.class, args);
     }
-
 }
