f76d4d90cd7f31152c7819e455d24821ff917611
ARJUN DAS <arjundasofficial012@gmail.com>
2025-04-03 18:40:18 +0530

    minor changes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Section from './components/Section'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Section/>
     </>
   )
 }
--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,21 +1,30 @@
 import React from "react";
-import assets from "../assets/assets";
+import { ArrowRight } from 'lucide-react';
+import { assets } from '../assets/assets';
 
 export default function Hero() {
   return (
-    <div className="relative h-screen w-full bg-cover bg-center flex items-center rounded-3xl overflow-hidden" 
-    style={{ backgroundImage: `url(${assets.})` }}>
-      
+    <div className="relative h-[800px] w-full flex items-center justify-left rounded-3xl overflow-hidden">
+      {/* Video Background */}
+      <video
+        className="absolute inset-0 w-full h-full object-cover"
+        src={assets.hero}
+        autoPlay
+        loop
+        muted
+        playsInline
+      />
+
       {/* Overlay */}
-      {/* <div className="absolute inset-0 bg-black bg-opacity-50"></div> */}
-      
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+
       {/* Content */}
-      <div className="relative z-10 text-white max-w-2xl px-10">
+      <div className="relative z-10 text-white max-w-2xl px-10 text-left items-center">
         <h1 className="text-5xl font-bold">Turnkey Pharmacy Fulfillment Solutions</h1>
         <p className="mt-3 text-lg">Pharmacy, Telehealth, Ready to Market</p>
         <button className="mt-6 px-6 py-3 bg-teal-500 hover:bg-teal-600 text-white font-semibold rounded-lg shadow-lg flex items-center gap-2">
           Get Started
-          <span className="material-icons">arrow_forward</span>
+          <span><ArrowRight /></span>
         </button>
       </div>
     </div>
