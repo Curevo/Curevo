fb817f4b1016e4d7892fb9f59c96d7f2e77e841d
Rounak665 <rounakcstar665@gmail.com>
2025-06-05 17:06:53 +0530

    Vercel Deploy Issue #4

--- Frontend/src/HealthCare/Pages/Contact.jsx
+++ Frontend/src/HealthCare/Pages/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavbarHome from '../Components/NavbarHome'
-import ContactCTA from '../components/contact/ContactCTA'
-import ContactFormSection from '../components/contact/ContactFormSection'
+import ContactCTA from '../Components/Contact/ContactCTA'
+import ContactFormSection from '../Components/Contact/ContactFormSection'
 import FooterHome from '../Components/FooterHome'
 
 
