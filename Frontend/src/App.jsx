d44ca6c02773091fb15b9a74a26439b1729753c0
ARJUN DAS <arjundasofficial012@gmail.com>
2025-06-10 21:05:17 +0530

    Added Delivery Executive Register page.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -27,56 +27,60 @@ import PrivacyPolicy from './Components/PrivacyPolicy'
 import Licenses from './Components/Licenses'
 
 import OrderDetails from './HealthCare/Components/CustomerDetailsAtOrder.jsx'
+import DeliveryExecutiveRegister from './Components/DeliveryExecutiveRegister.jsx'
 
 
 
 const App = () => {
 
       return (
-          <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/store/home" element={<HomeStore />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/doctors" element={<Doctors />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/products" element={<ProductGrid />} />
-                <Route path="/store/home" element={<HomeStore />} />
-                <Route path="/store/products" element={<ProductStore />} />
-                <Route path="/product/:productId/store/:storeId" element={<ProductDetailsPage />} />
+            <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/store/home" element={<HomeStore />} />
+                  <Route path="/services" element={<Services />} />
+                  <Route path="/doctors" element={<Doctors />} />
+                  <Route path="/about" element={<About />} />
+                  <Route path="/contact" element={<Contact />} />
+                  <Route path="/signup" element={<Signup />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/products" element={<ProductGrid />} />
+                  <Route path="/store/home" element={<HomeStore />} />
+                  <Route path="/store/products" element={<ProductStore />} />
+                  <Route path="/product/:productId/store/:storeId" element={<ProductDetailsPage />} />
 
 
 
-                <Route path="/userprofile" element={<UserProfile />} />
-                <Route path="/otp" element={<VerifyOTP />} />
-                <Route path="/doctor/:doctorId" element={<DoctorPreview />} />
-                <Route path="/appointments/:doctorId" element={<Appointments />} />
+                  <Route path="/userprofile" element={<UserProfile />} />
+                  <Route path="/otp" element={<VerifyOTP />} />
+                  <Route path="/doctor/:doctorId" element={<DoctorPreview />} />
+                  <Route path="/appointments/:doctorId" element={<Appointments />} />
 
 
 
-                <Route path="/payment/appointment/:appointmentId" element={<PaymentGateway />} />
-                <Route path="/payment/store/:orderId" element={<StorePaymentGateway />} />
-                <Route path="/store/payment" element={<StorePaymentGateway />} />
+                  <Route path="/payment/appointment/:appointmentId" element={<PaymentGateway />} />
+                  <Route path="/payment/store/:orderId" element={<StorePaymentGateway />} />
+                  <Route path="/store/payment" element={<StorePaymentGateway />} />
 
 
 
-                <Route path="/payment/store" element={<StorePaymentGateway />} />
-                <Route path="/admin/home" element={<AdminLanding/>} />
-                <Route path="/admin/products" element={<ProductManageAdmin/>} />
-                <Route path="/admin/store" element={<StoreManageAdmin/>} />
-                <Route path="/admin/clinics" element={<ClinicManageAdmin/>} />
-                <Route path="/admin/appointments" element={<AppointmentManageAdmin/>} />
+                  <Route path="/payment/store" element={<StorePaymentGateway />} />
+                  <Route path="/admin/home" element={<AdminLanding/>} />
+                  <Route path="/admin/products" element={<ProductManageAdmin/>} />
+                  <Route path="/admin/store" element={<StoreManageAdmin/>} />
+                  <Route path="/admin/clinics" element={<ClinicManageAdmin/>} />
+                  <Route path="/admin/appointments" element={<AppointmentManageAdmin/>} />
 
 
 
-                <Route path="/terms" element={<TermsAndConditions/>} />
-                <Route path="/policies" element={<PrivacyPolicy/>} />
-                <Route path="/licenses" element={<Licenses/>} />
-                <Route path="/orderDetails" element={<OrderDetails />} />
+                  <Route path="/terms" element={<TermsAndConditions/>} />
+                  <Route path="/policies" element={<PrivacyPolicy/>} />
+                  <Route path="/licenses" element={<Licenses/>} />
+                  <Route path="/orderDetails" element={<OrderDetails />} />
 
-          </Routes>
+
+                  <Route path="/Delivery/signup" element={<DeliveryExecutiveRegister />} />
+
+            </Routes>
       );
 
 };
